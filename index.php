593fc7f042fb78dc17a2075a13b1c577db88e7f9
giantsfan <thegurugamers@gmail.com>
2017-11-30 13:06:23 -0500

    Fix Fontawesome version

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
   <!-- Bootstrap core CSS-->
   <link href="css/bootstrap.min.css" rel="stylesheet">
   <!-- Custom fonts for this template-->
-  <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+  <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
   <!-- Custom styles for this template-->
   <link href="css/sb-admin.css" rel="stylesheet">
   <script src="js/pace.js"></script>
