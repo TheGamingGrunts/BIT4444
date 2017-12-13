aaa059659e2a231d6bfc67e475a266f0b7df1cb6
giantsfan <thegurugamers@gmail.com>
2017-12-12 22:16:54 -0500

    404 Error page

--- 404.php
+++ 404.php
@@ -37,7 +37,7 @@
     <div class="collapse navbar-collapse" id="navbarResponsive">
       <ul class="navbar-nav navbar-sidenav" id="exampleAccordion">
         <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Home">
-          <a class="nav-link" href="">
+          <a class="nav-link" href="index.php">
             <i class="fa fa-fw fa-home"></i>
             <span class="nav-link-text">Home</span>
           </a>
